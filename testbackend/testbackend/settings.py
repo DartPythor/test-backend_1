6fb5bfe7170254b4e51e6dc679016d90e50cb07a
Maxim <neverovm28@gmail.com>
2024-06-18 19:04:04 +0500

    fix: Fix errors with S3 storage

--- testbackend/testbackend/settings.py
+++ testbackend/testbackend/settings.py
@@ -63,7 +63,7 @@ if USE_S3:
     AWS_S3_ENDPOINT_URL = config("S3_HOST", cast=str)
     MEDIA_URL = f"{AWS_S3_ENDPOINT_URL}/media/"
     DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-    STATICFILES_STORAGE = "testbackend.storages.StaticRootS3BotoStorage"
+    STATICFILES_STORAGE = "testbackend.storage.StaticRootS3BotoStorage"
 else:
     DEFAULT_FILE_STORAGE = "django.core.files.storage.FileSystemStorage"
     MEDIA_URL = "/media/"
